06816ef58607fc2d701dc19ab4f5458823ef371a
robertjgoette <54007387+robertjgoette@users.noreply.github.com>
2021-07-07 16:25:44 -0500

    Removed redundancy in steps

--- steps/set_associate_scores_steps.py
+++ steps/set_associate_scores_steps.py
@@ -1,55 +1,25 @@
 from behave import given, when, then
 
-@given(u'The Instructor is on a page for a Python - 20200120')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given The Instructor is on a page for a Python - 20200120')
+@given('The Instructor is on a page for a {batch}')
+def step_impl(context, batch: str):
+    pass
 
 
-@when(u'The Instructor clicks on an Associate Zachary from a list')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When The Instructor clicks on an Associate Zachary from a list')
+@when(u'The Instructor clicks on an Associate <name> from a list')
+def step_impl(context, name: str):
+    pass
 
 
-@when(u'The Instructor sets a grade for an TEST')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When The Instructor sets a grade for an TEST')
+@when(u'The Instructor sets a grade for an <assessment>')
+def step_impl(context, assessment:str):
+    pass
 
 
 @when(u'The Instructor clicks the button to save the Score')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When The Instructor clicks the button to save the Score')
+    pass
 
 
 @then(u'A message should tell the Instructor that their grade was saved')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then A message should tell the Instructor that their grade was saved')
-
-
-@given(u'The Instructor is on a page for a iOS Development - 20210320')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given The Instructor is on a page for a iOS Development - 20210320')
-
-
-@when(u'The Instructor clicks on an Associate Kerry from a list')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When The Instructor clicks on an Associate Kerry from a list')
-
-
-@when(u'The Instructor sets a grade for an iOS Quiz')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When The Instructor sets a grade for an iOS Quiz')
-
-
-@given(u'The Instructor is on a page for a C++ - 20202222')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given The Instructor is on a page for a C++ - 20202222')
-
-
-@when(u'The Instructor clicks on an Associate Patrick from a list')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When The Instructor clicks on an Associate Patrick from a list')
-
-
-@when(u'The Instructor sets a grade for an Cumulative')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When The Instructor sets a grade for an Cumulative')
+    pass
