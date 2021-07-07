40bc1faab3042c85e24df82524d9202f1549f55a
mrussell481 <84339257+mrussell481@users.noreply.github.com>
2021-07-07 17:04:16 -0400

    Search Batch feature created along with implementation framework

---
+++ steps/search_batch_steps.py
@@ -0,0 +1,27 @@
+from behave import when, then
+
+
+'''Successful Search'''
+@when(u'The Instructor types Python into the Search Bar')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When The Instructor types Python into the Search Bar')
+
+@when(u'The Instructor types Java into the Search Bar')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When The Instructor types Java into the Search Bar')
+
+@when(u'The Instructor types C# into the Search Bar')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When The Instructor types C# into the Search Bar')
+
+@then(u'A list of results is displayed beneath the Search Bar')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: A list of results is displayed beneath the Search Bar')
+
+@when(u'The Instructor clicks on a result')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: The Instructor clicks on a result')
+
+@then(u'They are taken to the page for that Batch')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: The Instructor clicks on a result')
