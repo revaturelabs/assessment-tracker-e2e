3fc68bc263d9d2361f6c9e5156d9cef1a57cc0d4
robertjgoette <54007387+robertjgoette@users.noreply.github.com>
2021-07-08 14:54:56 -0500

    Full upload with working parts

--- poms/batch_home_page.py
+++ poms/batch_home_page.py
@@ -2,6 +2,18 @@
 
 from selenium.webdriver.chrome.webdriver import WebDriver
 
+# Variables for set_associate_scores_steps
+test_1_name: str = '//*[@id="associate1"]/a'
+test_1_assessment: str = "score18"
+test_1_save: str = '//*[@id="GiveScoreForm18"]/div[2]/button[1]'
+
+test_2_name: str = '//*[@id="associate11"]/a'
+test_2_assessment: str = "score15"
+test_2_save: str = '//*[@id="GiveScoreForm15"]/div[2]/button[1]'
+
+test_3_name: str = '//*[@id="associate10"]/a'
+test_3_assessment: str = "score16"
+test_3_save: str = '//*[@id="GiveScoreForm16"]/div[2]/button[1]'
 
 class BatchHomePage:
 
@@ -22,3 +34,33 @@ class BatchHomePage:
 
     def close_assessment_button(self):
         return self.driver.find_element_by_xpath('//*[@id="createAssessmentForm"]/div[4]/button[2]')
+
+    # For set_associate_scores_steps
+    def get_name_1(self):
+        return self.driver.find_element_by_xpath(test_1_name)
+
+    def get_name_2(self):
+        return self.driver.find_element_by_xpath(test_2_name)
+
+    def get_name_3(self):
+        return self.driver.find_element_by_xpath(test_3_name)
+
+    # For set_associate_scores_steps
+    def set_grade_1(self):
+        return self.driver.find_element_by_id(test_1_assessment)
+
+    def set_grade_2(self):
+        return self.driver.find_element_by_id(test_2_assessment)
+
+    def set_grade_3(self):
+        return self.driver.find_element_by_id(test_3_assessment)
+
+    # For set_associate_scores_steps
+    def save_button_1(self):
+        return self.driver.find_element_by_xpath(test_1_save)
+
+    def save_button_2(self):
+        return self.driver.find_element_by_xpath(test_2_save)
+
+    def save_button_3(self):
+        return self.driver.find_element_by_xpath(test_3_save)
--- poms/home_page.py
+++ poms/home_page.py
@@ -2,6 +2,12 @@
 
 from selenium.webdriver.chrome.webdriver import WebDriver
 
+# Variables for set_associate_scores_steps
+test_1_batch: str = '//*[@id="batch_2020"]/button[1]'
+
+test_2_batch: str = '//*[@id="batch_2021"]/button[2]'
+
+test_3_batch: str = '//*[@id="batch_2021"]/button[1]'
 
 class HomePage:
 
@@ -20,3 +26,16 @@ class HomePage:
     # The button for a user to click on to view that specific batches information
     def batch_button(self):
         return self.driver.find_element_by_xpath('//*[@id="batch_2020"]/button')
+
+    # For set_associate_scores_steps
+    # access the first button under a 2020 batch card
+    def batch_button_1(self):
+        return self.driver.find_element_by_xpath(test_1_batch)
+
+    # access the second button under a 2021 batch card
+    def batch_button_2(self):
+        return self.driver.find_element_by_xpath(test_2_batch)
+
+    # access the first button under a 2021 batch card
+    def batch_button_3(self):
+        return self.driver.find_element_by_xpath(test_3_batch)
