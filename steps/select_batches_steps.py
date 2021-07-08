c1e5d246b1983811f1d08d08eb222d84bc5e089f
wolfryan97 <wolf.ryan97@gmail.com>
2021-07-08 14:06:34 -0400

    fixed prev push
    accidentally hard coded value on last push v2

--- steps/select_batches_steps.py
+++ steps/select_batches_steps.py
@@ -17,4 +17,4 @@ def step_impl(context, batch: str):
 
 @then('The title should say {title}')
 def step_impl(context, title: str):
-    assert context.driver.title == str
+    assert context.driver.title == title
