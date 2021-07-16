4673927e6665fd2efb8c3f4ffa2f93422677bff6
Boston Bragg <bostonbragg@gmail.com>
2021-07-16 11:27:54 -0500

    instructor steps

--- steps/instructor_view_average_grade.py
+++ steps/instructor_view_average_grade.py
@@ -3,4 +3,4 @@ from behave import then
 
 @then(u'The average grade is displayed on the batch page')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then The average grade is displayed on the batch page')
+    assert context.batch_home_page.assessment_average().text.isnumeric()
